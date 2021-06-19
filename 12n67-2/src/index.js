628fe3d4b9b305c942eff31097da888774393335
Pachoy <pachoy@PachoydeMacBook-Air.local>
2021-06-19 12:18:00 +0800

    添加第一次 Commit

---
+++ 12n67-2/src/index.js
@@ -0,0 +1,64 @@
+import "./styles.css";
+
+let boxes = document.getElementsByClassName("box");
+let boxesArray = [...boxes];
+
+
+for (let i = 0; i < boxesArray.length; i++) {
+  let box = boxesArray[i];
+  box.onclick = (e) => {
+    for (let j = 0; j < boxesArray.length; j++) {
+      let _box = boxesArray[j];
+      if (_box.classList.contains("active")) {
+        _box.classList.remove("active");
+      }
+    }
+
+    e.target.classList.add("active");
+  };
+}
+
+// let box1 = document.getElementById("box1");
+// let box2 = document.getElementById("box2");
+// let box3 = document.getElementById("box3");
+// let box4 = document.getElementById("box4");
+// let box5 = document.getElementById("box5");
+
+// box1.onclick = () => {
+//   box1.classList.add("active");
+//   box2.classList.remove("active");
+//   box3.classList.remove("active");
+//   box4.classList.remove("active");
+//   box5.classList.remove("active");
+// };
+// box2.onclick = () => {
+//   box1.classList.remove("active");
+//   box2.classList.add("active");
+//   box3.classList.remove("active");
+//   box4.classList.remove("active");
+//   box5.classList.remove("active");
+
+// };
+// box3.onclick = () => {
+//   box1.classList.remove("active");
+//   box2.classList.remove("active");
+//   box3.classList.add("active");
+//   box4.classList.remove("active");
+//   box5.classList.remove("active");
+
+// };
+// box4.onclick = () => {
+//   box1.classList.remove("active");
+//   box2.classList.remove("active");
+//   box3.classList.remove("active");
+//   box4.classList.add("active");
+//   box5.classList.remove("active");
+
+// };
+// box5.onclick = () => {
+//   box1.classList.remove("active");
+//   box2.classList.remove("active");
+//   box3.classList.remove("active");
+//   box4.classList.remove("active");
+//   box5.classList.add("active");
+// };
